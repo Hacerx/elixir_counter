b24ff0115309d1d0cb590ac5de12872b234fbb7f
jsm134 <jsm134@alu.ua.es>
2018-11-17 02:15:06 +0100

    First Release

---
+++ Elixir_counter/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Elixir_counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ecounter = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tiempo = new System.Windows.Forms.Label();
+            this.por = new System.Windows.Forms.Label();
+            this.reboot = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.ayuda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.progressBar1.Location = new System.Drawing.Point(15, 89);
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(468, 51);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Value = 5;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // ecounter
+            // 
+            this.ecounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ecounter.Location = new System.Drawing.Point(435, 39);
+            this.ecounter.Name = "ecounter";
+            this.ecounter.Size = new System.Drawing.Size(45, 47);
+            this.ecounter.TabIndex = 1;
+            this.ecounter.Text = "5";
+            this.ecounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ecounter.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(396, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tiempo
+            // 
+            this.tiempo.AutoSize = true;
+            this.tiempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tiempo.Location = new System.Drawing.Point(13, 11);
+            this.tiempo.Name = "tiempo";
+            this.tiempo.Size = new System.Drawing.Size(70, 25);
+            this.tiempo.TabIndex = 3;
+            this.tiempo.Text = "label1";
+            // 
+            // por
+            // 
+            this.por.AutoSize = true;
+            this.por.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.por.Location = new System.Drawing.Point(12, 47);
+            this.por.Name = "por";
+            this.por.Size = new System.Drawing.Size(42, 31);
+            this.por.TabIndex = 4;
+            this.por.Text = "x1";
+            this.por.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.por.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // reboot
+            // 
+            this.reboot.Location = new System.Drawing.Point(15, 146);
+            this.reboot.Name = "reboot";
+            this.reboot.Size = new System.Drawing.Size(87, 33);
+            this.reboot.TabIndex = 5;
+            this.reboot.Text = "Reiniciar";
+            this.reboot.UseVisualStyleBackColor = true;
+            this.reboot.Click += new System.EventHandler(this.reboot_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // ayuda
+            // 
+            this.ayuda.Location = new System.Drawing.Point(393, 143);
+            this.ayuda.Name = "ayuda";
+            this.ayuda.Size = new System.Drawing.Size(87, 33);
+            this.ayuda.TabIndex = 6;
+            this.ayuda.Text = "Ayuda";
+            this.ayuda.UseVisualStyleBackColor = true;
+            this.ayuda.Click += new System.EventHandler(this.ayuda_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 188);
+            this.Controls.Add(this.ayuda);
+            this.Controls.Add(this.reboot);
+            this.Controls.Add(this.por);
+            this.Controls.Add(this.tiempo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ecounter);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Elixir Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label ecounter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label tiempo;
+        private System.Windows.Forms.Label por;
+        private System.Windows.Forms.Button reboot;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button ayuda;
+    }
+}
+
---
+++ Elixir_counter/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elixir_counter
+{
+    public partial class Form1 : Form
+    {
+        int minuto = 2;
+        int segundo = 60;
+        public Form1()
+        {
+            InitializeComponent();
+            tiempo.Text = "3:00";
+        }
+
+        protected override bool ProcessCmdKey(ref System.Windows.Forms.Message msg, System.Windows.Forms.Keys keyData)
+        { 
+            if (keyData == Keys.D1 || keyData == Keys.NumPad1 && progressBar1.Value >= 1 )
+            {
+                progressBar1.Value -= 1;
+            }
+            if (keyData == Keys.D2 || keyData == Keys.NumPad2 && progressBar1.Value >= 2)
+            {
+                progressBar1.Value -= 2;
+            }
+            if (keyData == Keys.D3 || keyData == Keys.NumPad3 && progressBar1.Value >= 3)
+            {
+                progressBar1.Value -= 3;
+            }
+            if (keyData == Keys.D4 || keyData == Keys.NumPad4 && progressBar1.Value >= 4)
+            {
+                progressBar1.Value -= 4;
+            }
+            if (keyData == Keys.D5 || keyData == Keys.NumPad5 && progressBar1.Value >= 5)
+            {
+                progressBar1.Value -= 5;
+            }
+            if (keyData == Keys.D6 || keyData == Keys.NumPad6 && progressBar1.Value >= 6)
+            {
+                progressBar1.Value -= 6;
+            }
+            if (keyData == Keys.D7 || keyData == Keys.NumPad7 && progressBar1.Value >= 7)
+            {
+                progressBar1.Value -= 7;
+            }
+            if (keyData == Keys.D8 || keyData == Keys.NumPad8 && progressBar1.Value >= 8)
+            {
+                progressBar1.Value -= 8;
+            }
+            if (keyData == Keys.D9 || keyData == Keys.NumPad9 && progressBar1.Value >= 9)
+            {
+                progressBar1.Value -= 9;
+            }
+            if(keyData == Keys.F1)
+            {
+                timer1.Enabled = timer2.Enabled = true;
+            }
+            if(keyData == Keys.F2)
+            {
+                timer1.Enabled = false;
+                minuto = 2;
+                segundo = 60;
+                por.Text = "x1";
+                progressBar1.Value = 5;
+                tiempo.Text = "3:00";
+                ecounter.Text = 5.ToString();
+            }
+            ecounter.Text = progressBar1.Value.ToString();
+            return true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = timer2.Enabled = true;   
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //segundo--;
+            if (minuto == 1 && segundo == 0)
+            {
+                timer1.Interval = 1400;
+                por.Text = "x2";
+            }
+
+            if (progressBar1.Value != 10)
+            {
+                progressBar1.Increment(1);
+                ecounter.Text = progressBar1.Value.ToString();
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reboot_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = timer2.Enabled = false;
+            minuto = 2;
+            segundo = 60;
+            por.Text = "x1";
+            progressBar1.Value = 5;
+            tiempo.Text = "3:00";
+            ecounter.Text = 5.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            segundo--;
+            if(minuto > 0 && segundo == 0)
+            {
+                segundo = 59;
+                minuto--;
+            }
+            if (minuto == 0 && segundo == 0)
+            {
+                timer1.Enabled = timer2.Enabled = false;
+            }
+            if (segundo > 9)
+            {
+                tiempo.Text = minuto + ":" + segundo;
+            }
+            else
+            {
+                tiempo.Text = minuto + ":0" + segundo;
+            }
+        }
+
+        private void ayuda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pulsa F1 para iniciar \n" +
+                "Pulsa F2 para reiniciar \n" +
+                "Puedes utilizar cualquier número para eliminar elixir", "Ayuda",
+                MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
